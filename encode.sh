d9c339c2bca7ebe960764c979c23708dc7097090
Takio Yamaoka <y.takio@gmail.com>
2024-06-15 01:00:56 +0100

    update with mjpeg

--- encode.sh
+++ encode.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 SIZE=800x600
 FPS=30
-HLS_TIME=5
+HLS_TIME=1
 HLS_SIZE=3
+AUDIO_FS=44100
 
 ffmpeg \
   -loglevel info \
-  -f v4l2 -input_format yuyv422 -s ${SIZE} -framerate ${FPS} -thread_queue_size 32 -i /dev/video0 \
-  -f alsa -channels 1 -thread_queue_size 4096 -ar 8000 -i plughw:1,0 \
+  -f v4l2 -input_format mjpeg -s ${SIZE} -framerate ${FPS} -thread_queue_size 32 -i /dev/video0 \
+  -f alsa -channels 1 -thread_queue_size 4096 -ar ${AUDIO_FS} -i plughw:1,0 \
   -c:a aac \
-  -ar 8000 \
+  -ar ${AUDIO_FS} \
   -ab 64k \
+  -pix_fmt nv12 \
   -c:v h264_v4l2m2m \
   -r ${FPS} \
-  -b:v 5M \
+  -b:v 2M \
   -bf 0 \
   -num_capture_buffers 32 \
   -f segment -segment_format mpegts \
